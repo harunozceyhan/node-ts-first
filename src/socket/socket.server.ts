674ddb78ed2622fb9a61e7e5380c159a28cb8942
Harun Ozceyhan <harunozceyhan@gmail.com>
2020-01-24 16:45:49 +0300

    feat: socket server add in progress

--- src/socket/socket.server.ts
+++ src/socket/socket.server.ts
@@ -2,29 +2,29 @@ import socketIo from 'socket.io'
 import { Server } from 'http'
 
 class SocketServer {
-    private io: SocketIO.Server
+	private io: SocketIO.Server
 
-    constructor(server: Server) {
-        this.io = socketIo(server)
-        this.listen()
-    }
+	constructor(server: Server) {
+		this.io = socketIo(server)
+		this.listen()
+	}
 
-    private listen(): void {
-        this.io.of('/default').on('connect', (socket: socketIo.Socket) => {
-            socket.emit('welcome', { message: 'welcome', id: socket.id })
-            socket.on('message', (m: string) => {
-                console.log('[server](message): %s', m)
-            })
+	private listen(): void {
+		this.io.of('/default').on('connect', (socket: socketIo.Socket) => {
+			socket.on('message', (m: string) => {
+				console.log('[server](message): %s', m)
+				socket.emit('welcome', { message: 'welcome', id: socket.id })
+			})
 
-            socket.on('disconnect', () => {
-                console.log('Client disconnected')
-            })
-        })
-    }
+			socket.on('disconnect', () => {
+				console.log('Client disconnected')
+			})
+		})
+	}
 
-    public getIo(): SocketIO.Server {
-        return this.io
-    }
+	public getIo(): SocketIO.Server {
+		return this.io
+	}
 }
 
 export default SocketServer
