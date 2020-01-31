6db1ec8767cbe93f33d36bde1ec3dffcfd09634e
Harun Ozceyhan <harunozceyhan@gmail.com>
2020-01-31 15:18:57 +0300

    fix: minor bug fixes

--- src/app.ts
+++ src/app.ts
@@ -2,40 +2,40 @@ import express from 'express'
 import { Server } from 'http'
 
 class App {
-    public app: express.Application
-    public port: number
+	public app: express.Application
+	public port: number
 
-    constructor(appInit: { port: number; middleWares: any; controllers: any }) {
-        this.app = express()
-        this.port = appInit.port
+	constructor(appInit: { middleWares: any; controllers: any }) {
+		this.app = express()
+		this.port = parseInt(process.env.PORT)
 
-        this.middlewares(appInit.middleWares)
-        this.routes(appInit.controllers)
-        this.template()
-    }
+		this.middlewares(appInit.middleWares)
+		this.routes(appInit.controllers)
+		this.template()
+	}
 
-    private middlewares(middleWares: { forEach: (arg0: (middleWare: any) => void) => void }) {
-        middleWares.forEach(middleWare => {
-            this.app.use(middleWare)
-        })
-    }
+	private middlewares(middleWares: { forEach: (arg0: (middleWare: any) => void) => void }) {
+		middleWares.forEach(middleWare => {
+			this.app.use(middleWare)
+		})
+	}
 
-    private routes(controllers: { forEach: (arg0: (controller: any) => void) => void }) {
-        controllers.forEach(controller => {
-            this.app.use('/', controller.router)
-        })
-    }
+	private routes(controllers: { forEach: (arg0: (controller: any) => void) => void }) {
+		controllers.forEach(controller => {
+			this.app.use('/', controller.router)
+		})
+	}
 
-    private template() {
-        this.app.use(express.json())
-        this.app.use(express.urlencoded({ extended: false }))
-    }
+	private template() {
+		this.app.use(express.json())
+		this.app.use(express.urlencoded({ extended: false }))
+	}
 
-    public listen(): Server {
-        return this.app.listen(this.port, () => {
-            console.log(`App listening on the http://localhost:${this.port}`)
-        })
-    }
+	public listen(): Server {
+		return this.app.listen(this.port, () => {
+			console.log(`App listening on the http://localhost:${this.port}`)
+		})
+	}
 }
 
 export default App
--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,13 @@ import SocketServer from './socket/socket.server'
 import loggerMiddleware from './middleware/logger'
 import HomeController from './controllers/home.controller'
 
-dotenv.config()
-const port = parseInt(process.env.PORT) || 3000
-const homeController = new HomeController()
+if (process.env.NODE_ENV !== 'production') {
+	dotenv.config()
+}
 
-const app = new App({
-	port: port,
-	controllers: [homeController],
-	middleWares: [bodyParser.json(), bodyParser.urlencoded({ extended: true }), loggerMiddleware]
-})
 new DBInitializer()
+const homeController = new HomeController()
+const app = new App({ controllers: [homeController], middleWares: [bodyParser.json(), bodyParser.urlencoded({ extended: true }), loggerMiddleware] })
 const server = app.listen()
 const socketServer = new SocketServer(server)
 homeController.setSocketServer(socketServer.getChatSocket())
